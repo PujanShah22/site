a7c7745d6096cdbceb321a7048f31e24283cd3f4
Pujan Shah <pujanshah22@gmail.com>
2020-06-14 22:16:00 +0530

    Updates

---
+++ precache-manifest.82c9676e22f3b11ab13aa249c32bcd52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f93523229eb2069134c",
+    "url": "/site/static/css/main.5abc90f9.chunk.css"
+  },
+  {
+    "revision": "0f93523229eb2069134c",
+    "url": "/site/static/js/main.0f935232.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/site/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "5571d5242120e713b47d",
+    "url": "/site/static/js/runtime~main.5571d524.js"
+  },
+  {
+    "revision": "417c5ce17a1efe04deca750903a7940c",
+    "url": "/site/index.html"
+  }
+];
